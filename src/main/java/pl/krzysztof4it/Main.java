38a19eb0819e2cea816677413152e131e036f944
krzysztof4it <krzysztof4it@gmail.com>
2017-07-06 18:17:48 +0200

    Add Resources bundldle - l18n + app title + name of buttons

--- src/main/java/pl/krzysztof4it/Main.java
+++ src/main/java/pl/krzysztof4it/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 public class Main extends Application{
 
@@ -17,12 +20,17 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Locale.setDefault(new Locale("en"));
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml")); // load file FXML
+
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+
         BorderPane borderPane = loader.load(); // element in fxml
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Moja Biblioteczka");
+        primaryStage.setTitle(bundle.getString("title.application"));
         primaryStage.show();
 
     }
