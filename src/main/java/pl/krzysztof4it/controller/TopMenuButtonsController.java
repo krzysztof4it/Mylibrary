7e3da65866ec63a73cba53315c427bb08f9cbf96
Krzysztof <krzysztof4it@gmail.com>
2017-07-12 15:13:59 +0200

    Add Menu with toggle buttons #26

--- src/main/java/pl/krzysztof4it/controller/TopMenuButtonsController.java
+++ src/main/java/pl/krzysztof4it/controller/TopMenuButtonsController.java
@@ -1,12 +1,17 @@
 package pl.krzysztof4it.controller;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 
 public class TopMenuButtonsController {
 
     private MainController mainController; /*Add reference */
+
+    @FXML
+    private ToggleGroup toggleGroup;
 
     public void setMainController(MainController mainController) { /*acces to chanege in main controller*/
         this.mainController = mainController;
@@ -24,5 +29,15 @@ public class TopMenuButtonsController {
     @FXML
     public void openStatistic() {
         System.out.println("Open Statistic");
+    }
+
+    @FXML
+    public void addBook() {
+
+        System.out.println("Add book");
+
+        if (toggleGroup.getSelectedToggle() != null){
+            toggleGroup.getSelectedToggle().setSelected(false); /*ustawia nie zaznaczony przycisk*/
+        }
     }
 }
